05b9a287fca53050e2392e2ef275f1ba1e1d758c
Marvin Reyes <reyemarv@gmail.com>
2021-05-25 04:59:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,5 @@
 # code your #position_taken? method here!
 
-# def position_taken?(board, index)
-#   if board[index - 1] != " " || nil 
-#     false 
-#   elsif board[index - 1] == "X" || "O"
-#     true 
-#   end 
-# end 
-
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
